ba8c62641ae78270c78ab6d25fadffa30ab35081
Axel García <111660292+Axelrpg@users.noreply.github.com>
2024-05-29 12:24:13 -0600

    Initial commit

---
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  templateUrl: './side-menu.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class SideMenuComponent {
+
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(':'))
+
+  constructor() {
+  }
+}
